45406b044146c3e0460660d422ccd11c29420616
이현근 <leehyunk6310@naver.com>
2023-09-01 12:50:32 +0900

    chore : 프로젝트 생성

---
+++ backend/src/main/java/com/prefin/PrefinApplication.java
@@ -0,0 +1,13 @@
+package com.prefin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrefinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrefinApplication.class, args);
+	}
+
+}
