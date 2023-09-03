fefdf231dacedd37f6f5d0a79937006987569d3e
know0329 <49915643+know0329@users.noreply.github.com>
2023-09-03 20:43:49 +0900

    Feat: 부모 홈 화면 어댑터 연결

--- mobile/Prefin/app/src/main/java/com/prefin/ui/parentHome/ParentHomeFragment.kt
+++ mobile/Prefin/app/src/main/java/com/prefin/ui/parentHome/ParentHomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.prefin.R
 import com.prefin.config.BaseFragment
 import com.prefin.databinding.FragmentParentHomeBinding
@@ -23,7 +24,7 @@ class ParentHomeFragment : BaseFragment<FragmentParentHomeBinding>(FragmentParen
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    private lateinit var adpater : ChildAccountAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,6 +35,25 @@ class ParentHomeFragment : BaseFragment<FragmentParentHomeBinding>(FragmentParen
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        init()
+    }
+
+    fun init(){
+
+        binding.apply {
+
+            // 눌렀을 때 계좌 거래 내역 조회
+            fragmentParentHomeMyAccountLinearLayout.setOnClickListener {
+
+            }
+
+            // adapter 연결
+            adpater = ChildAccountAdapter(requireContext())
+            fragmentParentHomeChildAccountRecyclerView.adapter = adpater
+            val layoutManager = LinearLayoutManager(requireContext())
+            fragmentParentHomeChildAccountRecyclerView.layoutManager = layoutManager
+        }
     }
 
     companion object {
