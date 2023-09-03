2d42128c7b6d7dbdff3e74d27417b7e9cc704687
spparty <65155903+leehyunk6310@users.noreply.github.com>
2023-09-03 21:27:00 +0900

    Feat: Child, Parent, Saving 기본 Controller,Service 추가

--- backend/src/main/java/com/prefin/repository/money/SavingRepository.java
+++ backend/src/main/java/com/prefin/repository/money/SavingRepository.java
@@ -3,5 +3,5 @@ package com.prefin.repository.money;
 import com.prefin.domain.money.SavingHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SavingHistoryRepository extends JpaRepository<SavingHistory, Long> {
+public interface SavingRepository extends JpaRepository<SavingHistory, Long> {
 }
