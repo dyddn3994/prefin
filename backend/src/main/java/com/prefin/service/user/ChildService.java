e32825382b8c5d1bd5db2041d7a0526ec39823ab
spparty <65155903+leehyunk6310@users.noreply.github.com>
2023-09-06 00:00:20 +0900

    Feat: 부모, 자녀 회원가입 기능 추가

--- backend/src/main/java/com/prefin/service/user/ChildService.java
+++ backend/src/main/java/com/prefin/service/user/ChildService.java
@@ -1,11 +1,43 @@
 package com.prefin.service.user;
 
+import com.prefin.domain.user.Child;
+import com.prefin.dto.user.ChildDto;
 import com.prefin.repository.user.ChildRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ChildService {
     private final ChildRepository childRepository;
+
+    // 자녀 회원 가입
+    public Long signUp(ChildDto child) {
+
+        Child newChild = Child.builder().
+                userId(child.getUserId()).
+                password(child.getPassword()).
+                name(child.getName()).
+                isQuizSolved(false).
+                quizId(0L).
+                build();
+
+        return childRepository.save(newChild).getId();
+    }
+
+    // 계좌 등록
+
+    // 간편 비밀번호 등록
+
+    // 저축 금액 변경
+
+    // 신뢰 점수 변경
+
+    // 부모 설정
+
+    // 마스코트 설정
+
+    // 퀴즈 설정
 }
--- backend/src/main/java/com/prefin/service/user/ParentService.java
+++ backend/src/main/java/com/prefin/service/user/ParentService.java
@@ -1,12 +1,84 @@
 package com.prefin.service.user;
 
+import com.prefin.domain.user.Parent;
+import com.prefin.dto.user.ParentDto;
 import com.prefin.repository.user.ParentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ParentService {
     private final ParentRepository parentRepository;
 
+    // 부모 회원 가입
+    public Long signUp(ParentDto parent) {
+
+        Parent newParent = Parent.builder().
+                userId(parent.getUserId()).
+                password(parent.getPassword()).
+                name(parent.getName()).
+                build();
+
+        return parentRepository.save(newParent).getId();
+    }
+
+    // 계좌 등록 설정
+    public Long setAccount(long id, String account) {
+        Parent parent = parentRepository.findById(id).orElse(null);
+
+        if (parent == null) return -1L;
+
+        parent.updateAccount(account);
+
+        return parentRepository.save(parent).getId();
+    }
+
+    // 간편 비밀번호 설정
+    public Long setSimplePassword(long id, String simplePassword) {
+        Parent parent = parentRepository.findById(id).orElse(null);
+
+        if (parent == null) return -1L;
+
+        parent.updateSimplePass(simplePassword);
+
+        return parentRepository.save(parent).getId();
+    }
+
+    // 대출 이율 설정
+    public Long setLoanRate(long id, BigDecimal loanRate) {
+        Parent parent = parentRepository.findById(id).orElse(null);
+
+        if (parent == null) return -1L;
+
+        parent.updateLoanRate(loanRate);
+
+        return parentRepository.save(parent).getId();
+    }
+
+    // 저축 이율 설정
+    public Long setSavingRate(long id, BigDecimal savingRate) {
+        Parent parent = parentRepository.findById(id).orElse(null);
+
+        if (parent == null) return -1L;
+
+        parent.updateSavingRate(savingRate);
+
+        return parentRepository.save(parent).getId();
+    }
+
+    // FCM Token 등록
+    public Long setToken(long id, String fcmToken) {
+        Parent parent = parentRepository.findById(id).orElse(null);
+
+        if (parent == null) return -1L;
+
+        parent.updateToken(fcmToken);
+
+        return parentRepository.save(parent).getId();
+    }
 }
