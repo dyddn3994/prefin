e31731ba232db046591b0382ecb883d47b839d1e
spparty <65155903+leehyunk6310@users.noreply.github.com>
2023-09-07 00:23:20 +0900

    Feat: 부모 컨트롤러 회원가입, 로그인 추가

--- backend/src/main/java/com/prefin/controller/user/ParentController.java
+++ backend/src/main/java/com/prefin/controller/user/ParentController.java
@@ -1,11 +1,26 @@
 package com.prefin.controller.user;
 
+import com.prefin.domain.user.Parent;
+import com.prefin.dto.user.ParentSignUpDto;
 import com.prefin.service.user.ParentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class ParentController {
     private final ParentService parentService;
+
+    @PostMapping("/parent")
+    public long signUp(@RequestBody ParentSignUpDto parentSignUpDto) {
+        return parentService.signUp(parentSignUpDto);
+    }
+
+    @PostMapping("/parent/login")
+    public Parent login(String userId, String password) {
+        return parentService.login(userId, password);
+    }
+
+
 }
