5434d8633f8911300cc9469e68288fb728cb9edb
spparty <65155903+leehyunk6310@users.noreply.github.com>
2023-09-07 00:20:37 +0900

    Feat: 부모, 자녀 userid로 쿼리하는 함수 추가

--- backend/src/main/java/com/prefin/repository/user/ChildRepository.java
+++ backend/src/main/java/com/prefin/repository/user/ChildRepository.java
@@ -4,7 +4,9 @@ import com.prefin.domain.user.Child;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ChildRepository extends JpaRepository<Child, Long> {
-
+    Optional<Child> findByUserId(String userId);
 }
--- backend/src/main/java/com/prefin/repository/user/ParentRepository.java
+++ backend/src/main/java/com/prefin/repository/user/ParentRepository.java
@@ -4,6 +4,9 @@ import com.prefin.domain.user.Parent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ParentRepository extends JpaRepository<Parent, Long> {
+    Optional<Parent> findByUserId(String userId);
 }
