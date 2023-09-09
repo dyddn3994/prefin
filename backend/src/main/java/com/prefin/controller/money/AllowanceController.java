12246d4a1b1ed35d7c5211493b7d0a72df1fafff
손준배 <thswnsqo99@gmail.com>
2023-09-10 02:37:35 +0900

    Fix: 대출 로직과 용돈 설정 및 이체 로직 오류 수정

--- backend/src/main/java/com/prefin/controller/money/AllowanceController.java
+++ backend/src/main/java/com/prefin/controller/money/AllowanceController.java
@@ -5,6 +5,7 @@ import com.prefin.service.money.AllowanceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,14 +17,12 @@ public class AllowanceController {
 
     // 용돈 수동 이체
     @PostMapping("/allowance/set")
-    public ResponseEntity<String> setAllowance(AllowanceDto requestDto) {
-        System.out.println("===============  testing  ==============");
+    public ResponseEntity<String> setAllowance(@RequestBody AllowanceDto requestDto) {
         return allowanceService.allowanceSetting(requestDto);
     }
 
     @PostMapping("/allowance/transfer")
-    public ResponseEntity<String> transferAllowance(AllowanceDto requestDto) {
-        System.out.println("===============  testing22222222222  ==============");
+    public ResponseEntity<String> transferAllowance(@RequestBody AllowanceDto requestDto) {
         return allowanceService.allowanceTransfer(requestDto);
     }
 
