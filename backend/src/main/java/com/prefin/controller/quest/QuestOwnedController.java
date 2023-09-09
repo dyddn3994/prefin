91290f7627dba794d633cd8684535e6621482815
이현근 <leehyunk6310@naver.com>
2023-09-09 23:19:07 +0900

    Feat: 퀘스트 소유 등록, 완료 요청, 완료 기능 추가

--- backend/src/main/java/com/prefin/controller/quest/QuestOwnedController.java
+++ backend/src/main/java/com/prefin/controller/quest/QuestOwnedController.java
@@ -1,11 +1,40 @@
 package com.prefin.controller.quest;
 
+import com.prefin.domain.quest.QuestOwned;
+import com.prefin.domain.user.Child;
+import com.prefin.dto.quest.QuestOwnedDto;
+import com.prefin.repository.quest.QuestOwnedRepository;
+import com.prefin.repository.user.ChildRepository;
+import com.prefin.service.quest.QuestOwnedService;
 import com.prefin.service.quest.QuestService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class QuestOwnedController {
-    private final QuestService questService;
+    private final QuestOwnedService questOwnedService;
+    private final QuestOwnedRepository questOwnedRepository;
+    private final ChildRepository childRepository;
+    @PostMapping("/questowned")
+    public void makeQuestOwned(@RequestBody QuestOwnedDto questOwnedDto) {
+        questOwnedService.makeQuestOwned(questOwnedDto);
+    }
+
+    @PutMapping("/questowned/request/{id}")
+    public void requestQuestComplete(@PathVariable long id) { questOwnedService.requestQuestComplete(id);}
+
+    @PutMapping("/questowned/complete/{id}")
+    public void setQuestCompleted(@PathVariable long id) { questOwnedService.setQuestCompleted(id);}
+
+    @GetMapping("/questowned/{id}")
+    public List<QuestOwned> getQuestOwnedByChild(long id) {
+        Child child = childRepository.findById(id).orElse(null);
+
+        return questOwnedRepository.findByChild(child);
+    }
+
 }
