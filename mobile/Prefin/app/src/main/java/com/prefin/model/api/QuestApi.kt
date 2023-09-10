7f8be0c0b7a0341d5b0cc95c7e7e1997c6ebcd44
dyddn3994 <dyddn3994@naver.com>
2023-09-10 19:42:57 +0900

    Feat: 자녀에게 등록된 퀘스트 조회 api 구현

--- mobile/Prefin/app/src/main/java/com/prefin/model/api/QuestApi.kt
+++ mobile/Prefin/app/src/main/java/com/prefin/model/api/QuestApi.kt
@@ -3,6 +3,7 @@ package com.prefin.model.api
 import com.prefin.model.dto.QuestRegisterRequest
 import com.prefin.model.dto.QuestCreateRequest
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
@@ -16,7 +17,15 @@ interface QuestApi {
 
     // 퀘스트 삭제
 
-    // 퀘스트 조회
+    // 생성된 퀘스트 조회
+    
+    
+    // 자녀에게 등록된 퀘스트 조회
+    // TODO: 반환값 설정 필요
+    @GET("questowned/{id}")
+    suspend fun childQuestList(
+        @Path("id") id: Long,
+    )
 
     // 퀘스트 등록
     @POST("questowned")
