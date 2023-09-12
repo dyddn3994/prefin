82d75519ebaf4b889ba32bb995462b071e2ab31a
손준배 <thswnsqo99@gmail.com>
2023-09-12 14:57:36 +0900

    Feat: 매일 00시에 실행되는 자동이체 로직 구현

--- backend/src/main/java/com/prefin/PrefinApplication.java
+++ backend/src/main/java/com/prefin/PrefinApplication.java
@@ -2,8 +2,10 @@ package com.prefin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PrefinApplication {
 
 	public static void main(String[] args) {
