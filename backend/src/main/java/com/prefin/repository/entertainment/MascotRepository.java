2fabe73ef7c34b97a5d2ed41441a0702b83edf79
이현근 <leehyunk6310@naver.com>
2023-09-13 22:15:26 +0900

    Remove: Mascot 삭제

--- backend/src/main/java/com/prefin/repository/entertainment/MascotRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prefin.repository.entertainment;
-
-import com.prefin.domain.entertainment.Mascot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MascotRepository extends JpaRepository<Mascot, Long> {
-}
