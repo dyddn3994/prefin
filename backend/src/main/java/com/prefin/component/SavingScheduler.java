56717519af97d58f0ccc9b4d7e49f1bf590868e0
손준배 <thswnsqo99@gmail.com>
2023-09-16 14:03:17 +0900

    Feat: 저축이자 자동 이체 로직 추가

--- backend/src/main/java/com/prefin/component/SavingScheduler.java
+++ backend/src/main/java/com/prefin/component/SavingScheduler.java
@@ -1,5 +1,6 @@
 package com.prefin.component;
 
+import com.prefin.domain.money.SavingHistory;
 import com.prefin.domain.user.Child;
 import com.prefin.repository.money.SavingRepository;
 import com.prefin.repository.user.ChildRepository;
@@ -9,25 +10,52 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class SavingScheduler {
     private final ChildRepository childRepository;
-    private final ParentRepository parentRepository;
     private final SavingRepository savingRepository;
 
-//    // 매 1일 이자 입금
-//    @Transactional
-//    @Scheduled(cron = "0 * * * * ?", zone = "Asia/Seoul")  // "0 * * * * ?" 매 분, "* 0 0 * * ?" 매 자정
-//    public void MonthlyInterest() {
-//        // 자녀의 저축금액으로 부모의 이율을 적용해서 돈을 준다.
-//        List<Child> childList = childRepository.findAll();
-//
-//        for (Child child : childList) {
-//            child.getSavingAmount()
-//        }
-//        // 저축 내역에 추가한다.
-//    }
+    // 매 1일 이자 입금
+    @Transactional
+    @Scheduled(cron = "0 * * * * ?", zone = "Asia/Seoul")  // "0 * * * * ?" 매 분
+    public void MonthlyInterest() {
+        // 자녀의 저축금액으로 부모의 이율을 적용해서 돈을 준다.
+        List<Child> childList = childRepository.findAll();
+
+        // for문 돌면서 각 child의 저금 금액 가져와서 부모의 savingRate로 이자를 계산한다
+        for (Child child : childList) {
+            int savedMoney = child.getSavingAmount();
+            if (savedMoney != 0) {
+                BigDecimal savingInterst = child.getSavingRate();
+                BigDecimal convertedSM = new BigDecimal(savedMoney);
+
+                BigDecimal result = savingInterst.multiply(convertedSM);
+                int monthlyInterest = result.setScale(0, RoundingMode.FLOOR).intValue();
+                if (monthlyInterest > child.getParent().getMaxSavingAmount()) {
+                    monthlyInterest = child.getParent().getMaxSavingAmount();
+                }
+
+                // 저축 금액에 이자 추가
+                child.updateSavingAmount(monthlyInterest);
+
+                // 저축 내역에 추가한다.
+                SavingHistory savingHistory = SavingHistory.builder()
+                        .savingAmount(child.getSavingAmount())
+                        .child(child)
+                        .parent(child.getParent())
+                        .savingDate(LocalDateTime.now().atZone(ZoneId.of("Asia/Seoul")).toInstant().toEpochMilli())
+                        .savingType("INTEREST")
+                        .build();
+
+                savingRepository.save(savingHistory);
+            }
+        }
+    }
 }
