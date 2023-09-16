eaf8863f21e7387e769c2f23f0fba50b4672a3a3
이현근 <leehyunk6310@naver.com>
2023-09-16 23:39:20 +0900

    Fix: 매일 정각에 스케줄러를 통해 유효기간이 지난 퀘스트 만료시키기

--- backend/src/main/java/com/prefin/component/QuestScheduler.java
+++ backend/src/main/java/com/prefin/component/QuestScheduler.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
@@ -18,15 +19,17 @@ public class QuestScheduler {
     private final QuestOwnedRepository questOwnedRepository;
 
     // 매 정각 퀘스트 갱신
+    // 현재 날짜가 16일이면 endDate가 15인 녀석들을 다 비활성화시킨다.
     @Transactional
-    @Scheduled(cron = "0 0 0 * * ?", zone = "Asia/Seoul")  // "0 * * * * ?" 매 분, "* 0 0 * * ?" 매 자정
+    @Scheduled(cron = "0 * * * * ?", zone = "Asia/Seoul")  // "0 * * * * ?" 매 분, "* 0 0 * * ?" 매 자정
     public void RenewQuest() {
         // 정각 시간을 들고온다.
-        Long endDate = LocalDateTime.now().atZone(ZoneId.of("Asia/Seoul")).toInstant().toEpochMilli();
-//        Long endDate = 1694827885543L;
-        List<QuestOwned> questOwnedList = questOwnedRepository.findByTime(endDate);
+//        Long endDate = LocalDateTime.now().minusDays(1).atZone(ZoneId.of("Asia/Seoul")).toInstant().toEpochMilli();
 
-        System.out.println("퀘스트 완료");
+        Long targetDate = LocalDateTime.now().minusDays(1).atZone(ZoneId.of("Asia/Seoul")).toInstant().toEpochMilli();
+        List<QuestOwned> questOwnedList = questOwnedRepository.findByTime(targetDate);
+        System.out.println(targetDate);
+
         // 등록된 퀘스트를 완료로 처리
         // 퀘스트 등록을 false로 변경
         for (QuestOwned questOwned : questOwnedList) {
