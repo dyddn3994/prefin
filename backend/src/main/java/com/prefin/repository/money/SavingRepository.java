3d92a983a82184bca446cec1a16a6998ad688338
이현근 <leehyunk6310@naver.com>
2023-09-16 10:17:12 +0900

    Feat: 자녀 아이디로 저축 내역 조회

--- backend/src/main/java/com/prefin/repository/money/SavingRepository.java
+++ backend/src/main/java/com/prefin/repository/money/SavingRepository.java
@@ -1,9 +1,19 @@
 package com.prefin.repository.money;
 
 import com.prefin.domain.money.SavingHistory;
+import com.prefin.domain.quest.QuestOwned;
+import com.prefin.domain.user.Child;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SavingRepository extends JpaRepository<SavingHistory, Long> {
+    @Query(value = "select savingHistory " +
+            "from SavingHistory savingHistory " +
+            "where savingHistory.child = :child")
+    List<SavingHistory> findByChild(@Param("child") Child child);
 }
